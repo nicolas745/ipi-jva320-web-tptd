bc97fe4347e0e340ee3ce158f204cc58bc266e52
nicolas blanc <47307856+nicolas745@users.noreply.github.com>
2023-12-06 12:48:09 +0100

    6/12/2023 12:48

--- src/main/java/com/ipi/jva320/Jva320Application.java
+++ src/main/java/com/ipi/jva320/Jva320Application.java
@@ -8,6 +8,5 @@ public class Jva320Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Jva320Application.class, args);
-    }
-
+    } 
 }
--- src/main/java/com/ipi/jva320/controller/SalaireController.java
+++ src/main/java/com/ipi/jva320/controller/SalaireController.java
@@ -1,5 +1,7 @@
 package com.ipi.jva320.controller;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -15,12 +17,17 @@ public class SalaireController {
 	@GetMapping(value = "/salaries/{id}")
 	public String home(@PathVariable int id,final ModelMap model) {
 		long nbsalaire = salarieAideADomicileService.countSalaries();
-		SalarieAideADomicile salarier = salarieAideADomicileService.getSalaries().get(id-1);
+		SalarieAideADomicile salarier = salarieAideADomicileService.getSalaries().get(id);
 		model.put("nbsalaire", nbsalaire);
 		model.put("id", id);
-		System.out.println(salarier.getNom());
 		model.put("nom",salarier.getNom());
-		model.put("prenom", "test");
+		model.put("moisCour", salarier.getMoisEnCours());
+		model.put("moisDebutContrat", salarier.getMoisDebutContrat());
+		model.put("joursTravaillesAnneeN", salarier.getJoursTravaillesAnneeN());
+		model.put("congesPayesAcquisAnneeN", salarier.getCongesPayesAcquisAnneeN());
+		model.put("joursTravaillesAnneeNMoins1", salarier.getJoursTravaillesAnneeNMoins1());
+		model.put("congesPayesAcquisAnneeNMoins1", salarier.getCongesPayesAcquisAnneeNMoins1());
+		model.put("congesPayesPrisAnneeNMoins1", salarier.getCongesPayesPrisAnneeNMoins1());
 		return "detail_Salarie";
 	}
 }

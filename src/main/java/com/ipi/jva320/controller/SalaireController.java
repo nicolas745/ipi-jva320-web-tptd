039c7ee887323a1ebed8bf7666045a3406728e77
nicolas blanc <47307856+nicolas745@users.noreply.github.com>
2023-12-06 10:27:44 +0100

    6/12/23 10:27

--- src/main/java/com/ipi/jva320/controller/SalaireController.java
+++ src/main/java/com/ipi/jva320/controller/SalaireController.java
@@ -4,18 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.ipi.jva320.model.SalarieAideADomicile;
 import com.ipi.jva320.service.SalarieAideADomicileService;
-
 @Controller
-public class HomeController {
+public class SalaireController {
 	@Autowired
 	private SalarieAideADomicileService salarieAideADomicileService;
-	@GetMapping(value = "/")
-	public String home(final ModelMap model) {
+	@GetMapping(value = "/salaries/{id}")
+	public String home(@PathVariable int id,final ModelMap model) {
 		long nbsalaire = salarieAideADomicileService.countSalaries();
+		SalarieAideADomicile salarier = salarieAideADomicileService.getSalaries().get(id-1);
 		model.put("nbsalaire", nbsalaire);
-		return "home";
+		model.put("id", id);
+		System.out.println(salarier.getNom());
+		model.put("nom",salarier.getNom());
+		model.put("prenom", "test");
+		return "detail_Salarie";
 	}
-
 }

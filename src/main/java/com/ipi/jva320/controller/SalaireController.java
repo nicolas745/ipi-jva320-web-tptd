aee412d6a835ab74a179efe6c1357ead8ec59b81
nicolas blanc <47307856+nicolas745@users.noreply.github.com>
2023-12-07 14:29:57 +0100

    7/12/23

--- src/main/java/com/ipi/jva320/controller/SalaireController.java
+++ src/main/java/com/ipi/jva320/controller/SalaireController.java
@@ -1,12 +1,14 @@
 package com.ipi.jva320.controller;
-
-import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ipi.jva320.model.SalarieAideADomicile;
 import com.ipi.jva320.service.SalarieAideADomicileService;
@@ -16,18 +18,30 @@ public class SalaireController {
 	private SalarieAideADomicileService salarieAideADomicileService;
 	@GetMapping(value = "/salaries/{id}")
 	public String home(@PathVariable int id,final ModelMap model) {
-		long nbsalaire = salarieAideADomicileService.countSalaries();
-		SalarieAideADomicile salarier = salarieAideADomicileService.getSalaries().get(id);
-		model.put("nbsalaire", nbsalaire);
-		model.put("id", id);
-		model.put("nom",salarier.getNom());
-		model.put("moisCour", salarier.getMoisEnCours());
-		model.put("moisDebutContrat", salarier.getMoisDebutContrat());
-		model.put("joursTravaillesAnneeN", salarier.getJoursTravaillesAnneeN());
-		model.put("congesPayesAcquisAnneeN", salarier.getCongesPayesAcquisAnneeN());
-		model.put("joursTravaillesAnneeNMoins1", salarier.getJoursTravaillesAnneeNMoins1());
-		model.put("congesPayesAcquisAnneeNMoins1", salarier.getCongesPayesAcquisAnneeNMoins1());
-		model.put("congesPayesPrisAnneeNMoins1", salarier.getCongesPayesPrisAnneeNMoins1());
+		SalarieAideADomicile salarier = salarieAideADomicileService.getSalaries().get(id-1);
+		model.addAttribute("salarie", salarier);
 		return "detail_Salarie";
+	}
+	
+	@GetMapping(value ="/salaries/aide/new")
+	public String salariesnew(final ModelMap model) {
+		SalarieAideADomicile salarier = new SalarieAideADomicile();
+		model.addAttribute("salarie", salarier);
+		return "detail_Salarie";
+	}
+	@PostMapping(value = "/salaries/aide/new")
+	public String postsalariesnew(@RequestParam Map<String, String> post, final ModelMap model) {
+		SalarieAideADomicile salarier = new SalarieAideADomicile();
+		System.out.println(post.toString());
+		model.addAttribute("salarie", salarier);
+		return "detail_Salarie";
+	}
+	@GetMapping(value = "/salaries")
+	public String salaries(@RequestParam("page") int page, @RequestParam("size") Long size, @RequestParam("sortProperty") String nom,@RequestParam("sortDirection") String sortDirection, final ModelMap model) {
+		List<SalarieAideADomicile> listsalaries = salarieAideADomicileService.getSalaries();
+		model.addAttribute("salaries",listsalaries); 
+		model.put("next", page+1);
+		model.put("last", page-1);
+		return "list"; 
 	}
 }
